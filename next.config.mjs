1b40938cf4137ec5ac3a576515e3297bbf659e08
Quinta <0pietroquintavalle0@gmail.com>
2024-06-12 23:39:46 +0200

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Other Next.js configuration options
+export default {
     output: 'export',
-    exportPathMap: async function (
-        defaultPathMap,
-        { dev, dir, outDir, distDir, buildId }
-    ) {
-        return {
-            '/': { page: '/' },
-            // Add other pages here if needed
-        };
-    },
-}
-
-export default nextConfig;
+    // Remove exportPathMap if it's present
+};
