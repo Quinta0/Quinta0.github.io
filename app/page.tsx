ee1d05f6dc5b0f4f7bb84fe0448d8a2c22369f8d
Pietro Quintavalle <0pietroquintavalle0@gmail.com>
2024-06-13 16:06:22 +0200

    fixed phone screen view of carousel

--- app/page.tsx
+++ app/page.tsx
@@ -245,7 +245,7 @@ export default function Component() {
 
         <section className="w-full py-12 md:py-24 lg:py-32 flex items-center justify-center">
           <div className="container px-4 md:px-6 flex flex-col items-center justify-center">
-            <div className="space-y-4 text-center">
+            <div className="space-y-4 text-center w-full">
               <div className="inline-block rounded-lg bg-gray-800 px-3 py-1 text-sm">Projects</div>
               <h2 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
                 My Recent Work
